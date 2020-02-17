3458e0bb83c72bd0f1d8d0dcd3fb45d3ca29178b
dmaish <mainadaniel81@gmail.com>
2020-02-17 17:34:43 +0300

    create user POST method returns new user obj as response

--- src/modules/user/index.js
+++ src/modules/user/index.js
@@ -5,14 +5,17 @@ export default class UserController {
   static async createNewUser(req, res) {
     try {
       const { username, email, password } = req.body;
-      await firebaseObj.database().ref(`/${username}`).set({
+      return await firebaseObj.database().ref(`/${username}`).set({
         username,
         email,
         password,
-      });
+      })
+        .then(() => {
+          firebaseObj.database().ref(`/${username}`).once('value').then((snap) => res.status(201).json(snap.val()));
+        });
     // eslint-disable-next-line no-empty
     } catch (error) {
-
+      return null;
     }
   }
 }
